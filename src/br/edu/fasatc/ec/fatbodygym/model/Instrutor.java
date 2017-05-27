031ab418948cac66ff77ba9a834c11078aaad072
Bruno Bitencourt Luiz <brunowdev@gmail.com>
2017-05-27 18:02:52 -0300

    builder no instrutor

--- src/br/edu/fasatc/ec/fatbodygym/model/Instrutor.java
+++ src/br/edu/fasatc/ec/fatbodygym/model/Instrutor.java
@@ -1,7 +1,10 @@
 package br.edu.fasatc.ec.fatbodygym.model;
 
+import java.util.Date;
+
 import br.edu.fasatc.ec.fatbodygym.constansts.ErpDatabaseConstants;
 import br.edu.fasatc.ec.fatbodygym.constansts.LocalFileAsTable;
+import br.edu.fasatc.ec.fatbodygym.model.common.EntityBuilder;
 
 @LocalFileAsTable(tableName = ErpDatabaseConstants.TABLE_INSTRUTORES)
 public final class Instrutor extends Pessoa implements SearchableString {
@@ -19,5 +22,43 @@ public final class Instrutor extends Pessoa implements SearchableString {
 	public String[] getSearchableFields() {
 		return new String[] { getCpf(), getNome(), getRg() };
 	}
+	
+
+	public static class Builder extends EntityBuilder<Instrutor> {
+		
+		protected Builder(Instrutor instrutor) {
+			super(instrutor);
+		}
+
+		public static Builder create() {
+			return new Builder(new Instrutor());
+		}
+
+		public static Builder from(Instrutor instrutor) {
+			return new Builder(instrutor);
+		}
+
+		public Builder nome(String nome) {
+			entity.setNome(nome);
+			return this;
+		}
+		
+		public Builder cpf(String cpf) {
+			entity.setCpf(cpf);
+			return this;
+		}
+		
+		public Builder dataNascimento(Date dataNascimento) {
+			entity.setDataNascimento(dataNascimento);
+			return this;
+		}
+		
+		public Builder rg(String rg) {
+			entity.setRg(rg);
+			return this;
+		}
+	
+	}
+
 
 }
